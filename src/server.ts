fc939976e75effd1719b980bb07dfa3600a9db21
William Sedlacek <wsedlacekc@gmail.com>
2019-11-05 11:59:47 -0600

    Setup Route
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import * as express from 'express';
 import * as helmet from 'helmet';
 
+import { carRouter } from './routes/car.routes';
+
 export const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use('/car', carRouter);
